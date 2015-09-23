1e3ccead50e685aefc9a8b0bdf6c5de22d7c498c
Johanna <jnamla@gmail.com>
2015-09-23 06:30:00 +0000

    mongodb link to Neil's database

--- bin/config.js
+++ bin/config.js
@@ -3,7 +3,7 @@ var config = {};
 //config.mongoUri = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/cymplar';
 //uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||  "mongodb://neilNeuli:neil2015@ds043981.mongolab.com:43981/cymplar"
 config.mongodb = {
-  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||  "mongodb://localhost:27017/cymplar"
+  uri: process.env.MONGOLAB_URI || process.env.MONGOHQ_URL ||  "mongodb://root:password@ds043981.mongolab.com:43981/cymplar"
 };
 
 config.secretKey = "cymplarSecret";
